18d3eb947d8c6ea62acad29e28c0b53e30961e82
yyd003 <yyd003@gmail.com>
2016-07-20 16:29:06 +0800

    ADD Servlet

---
+++ src/com/neuedu/my12306/usermgr/servlet/RegisterServlet.java
@@ -0,0 +1,36 @@
+package com.neuedu.my12306.usermgr.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegisterServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
